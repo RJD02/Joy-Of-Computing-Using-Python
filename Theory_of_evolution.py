acd57605c764343f097721c849ae42c40259c8f3
egnaluD-jarivaR <redhood.dumb@gmail.com>
2021-02-04 12:52:38 +0530

    Week 3 Program Theory Of Evolution

---
+++ Theory_of_evolution.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  4 12:28:17 2021
+
+@author: Dulange
+"""
+import random
+
+def evolve(x):
+    # randint(a, b) generates random integers in range [a, b]
+    index = random.randint(0, len(x) - 1)
+    p = random.randint(1, 100)
+    print(p)
+    if p == 1:
+        if x[index] == '0':
+            x[index] = '1'
+        else:
+            x[index] = '0'
+
+# x = []
+with open("in.txt", "r") as input:
+    # x.append(input.read())
+    x = input.read()
+    x = list(x)
+    
+for i in range(10000):
+    evolve(x)
+    
+print(x)
