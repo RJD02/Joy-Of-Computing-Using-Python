ff3e950db0457f6b47c80de27a863dd277bf781c
egnaluD-jarivaR <redhood.dumb@gmail.com>
2021-02-15 19:52:26 +0530

    Code for weekly tutorial

---
+++ Follow Alongs/fizzbuzz.py
@@ -0,0 +1,13 @@
+def fizzBuzz(n):
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print(i,'FizzBuzz')
+        elif i % 3 == 0:
+            print(i,'Fizz')
+        elif i % 5 == 0:
+            print(i,'Buzz')
+        else:
+            print(i)
+
+n = int(input())
+print(fizzBuzz(n))
