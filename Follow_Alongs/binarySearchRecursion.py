eadc19fc3c9d24b89b8ce0981c09e8b6f79bd5c3
egnaluD-jarivaR <redhood.dumb@gmail.com>
2021-02-22 12:43:02 +0530

    Week 6 Binary Search Recursive Code

---
+++ Follow_Alongs/binarySearchRecursion.py
@@ -0,0 +1,26 @@
+# Binary Search using Recursion
+def binarySearch(A, key, start, end):
+    if start == end:
+        if A[start] == key:
+            return start
+        else:
+            return -1
+    mid = start + (end - start) // 2
+    if A[mid] < key:
+        return binarySearch(A, key, mid + 1, end)
+    elif A[mid] > key:
+        return binarySearch(A, key, start, mid - 1)
+    elif A[mid] == key:
+        return mid
+    
+# l = [x for x in range(1, 10000000)]
+l = list(input().split(" "))
+for i in range(len(l)):
+    l[i] = int(l[i])
+l.sort()
+key = int(input())
+print('Sorted list is:', l, '\nValue', key, 'is at position')
+try:
+    print(1 + binarySearch(l, key, 0, len(l)))
+except RecursionError:
+    print(-1)
