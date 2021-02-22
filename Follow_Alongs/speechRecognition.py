7f3f54abcd5531af7d5da4815096a45167b8a09d
egnaluD-jarivaR <redhood.dumb@gmail.com>
2021-02-22 12:55:04 +0530

    Week 5 Speech Recognition Code

---
+++ Follow_Alongs/speechRecognition.py
@@ -0,0 +1,18 @@
+def run():
+    import speech_recognition as sr
+    AUDIO_FILE = ("back-in-black-official-video.wav")
+    #User audio
+    r = sr.Recognizer() # Initialize the recognizer
+    
+    with sr.AudioFile(AUDIO_FILE) as source:
+            audio = r.record(source)
+    # Reads the audio file
+    
+    try:
+        print("Audio file contains ", r.recognize_google(audio))
+    except sr.UnknownValueError:
+        print("Google Speech could not understand audio")
+    except sr.RequestError:
+        print("Couldn't get result from Google Speech Recogniztion")
+    
+
