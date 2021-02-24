8f2e2a132c0667c12a965fca0c554af193a91a7b
egnaluD-jarivaR <redhood.dumb@gmail.com>
2021-02-24 21:11:35 +0530

    Update Week 6 Substitution Cipher Code

--- Follow_Alongs/substitutionCipher.py
+++ Follow_Alongs/substitutionCipher.py
@@ -55,7 +55,6 @@ def encryptFromFile(shift):
     print('Encrypted Form:',data)
 
 def encryptFromInput(line, shift):
-    #  line = input()
     d = {}
     for i in range(len(string.ascii_letters)):
         d[string.ascii_letters[i]] = string.ascii_letters[i - shift]
