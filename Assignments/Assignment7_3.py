25e19afa0fb82145045af7105afb874244aa41e5
egnaluD-jarivaR <redhood.dumb@gmail.com>
2021-03-03 23:50:22 +0530

    Update Week 7 Assignment 3

--- Assignments/Assignment7_3.py
+++ Assignments/Assignment7_3.py
@@ -1,14 +1,8 @@
 def solve():
     line_input = input().split(",")
-    pair = [i.split(":") for i in line_input]
-    for i in range(len(pair)):
-        pair[i][1] = pair[i][1].strip()
-    for i in range(len(pair)):
-        for j in range(2):
-            pair[i][j] = int(pair[i][j])
+    pair = [i.split(":  ") for i in line_input]
+    pair = [[int(i[0]), int(i[1])] for i in pair]
     dice = input().split(", ")
-    for i in range(len(dice), 2):
-        dice[i] = dice[i].strip()
     dice = [int(x) for x in dice]
     count = 0
     for i in dice:
