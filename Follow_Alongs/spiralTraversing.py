2ba54b059a2e8966bef81fb7889bc5fc18dff603
egnaluD-jarivaR <redhood.dumb@gmail.com>
2021-03-04 21:51:51 +0530

    Add colors in drawing

--- Follow_Alongs/spiralTraversing.py
+++ Follow_Alongs/spiralTraversing.py
@@ -14,27 +14,32 @@ def spiral(row, col): #  mat = Matrix(list of list), row = number of rows, col =
     flag = 0
     leo.color("white")
     while r < row and c < col:
+        #  leo.color("red")
         if flag == 1:
             leo.right(90) #  Rotating leo by 90 degrees
         for i in range(c, col): #  Printing the first row from remaining rows
+            leo.color("orange")
             leo.forward(dist)
             #  print(mat[r][i], end = " ")
         r += 1
         flag = 1
         leo.right(90)
         for i in range(r, row): #  Printing the last column from remaining cloumn
+            leo.color("green")
             leo.forward(dist)
             #  print(mat[i][col - 1], end = " ")
         col -= 1
         leo.right(90)
         if r < row:
             for i in range(col - 1, c - 1, -1):
+                leo.color("orange")
                 leo.forward(dist)
                 #  print(mat[row - 1][i], end = " ")
             row -= 1
         leo.right(90)
         if c < col:
             for i in range(row - 1, r - 1, -1):
+                leo.color("green")
                 leo.forward(dist)
                 #  print(mat[i][c], end = " ")
             c += 1
