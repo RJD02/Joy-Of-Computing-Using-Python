22ca3048d230f25797e328ba3dd2d5cfb83635a4
egnaluD-jarivaR <redhood.dumb@gmail.com>
2021-03-10 18:45:44 +0530

    Add dynamic input and output of files

--- Follow_Alongs/imageProcessing.py
+++ Follow_Alongs/imageProcessing.py
@@ -2,12 +2,17 @@
 from PIL import Image
 
 #  Opening the image
-img = Image.open('kids-room.jpg')
+img_input = input('Enter the file name that you want to flip: ')
+#  img = Image.open('kids-room.jpg')
+img = Image.open(img_input)
 
 #  Transposing the image
 transposed_image = img.transpose(Image.FLIP_LEFT_RIGHT)
 
 #  Save the image to a file in a human understandable format
-transposed_image.save('kids-room-transposed.jpg')
+#  transposed_image.save('kids-room-transposed.jpg')
+#  print(img_input[:-4] + '-transposed' + img_input[-4:])
+
+transposed_image.save(img_input[:-4] + '-transposed' + img_input[-4:])
 
 print('Done flipping')
