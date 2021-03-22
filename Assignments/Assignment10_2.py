ae134318bcae5b2be9aab3aaff7a41480788f18e
egnaluD-jarivaR <redhood.dumb@gmail.com>
2021-03-22 16:59:09 +0530

    Add Week 10 Assignment 2 Code

---
+++ Assignments/Assignment10_2.py
@@ -0,0 +1,24 @@
+def solve():
+    n = int(input())
+    matrix = [[] for i in range(n)]
+    l = list(map(int, input().split()))
+    count = 0
+    for i in range(n):
+        for j in range(n):
+            matrix[i].append(l[count])
+            count += 1
+    #  print(matrix)
+    d = {x:0 for x in range(n)}
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == 1:
+                d[j] += 1
+    #  print(d)
+    for i in d.keys():
+        if d[i] == 2:
+            print('yes', i + 1)
+            return
+    print('no')
+    return
+
+solve()
