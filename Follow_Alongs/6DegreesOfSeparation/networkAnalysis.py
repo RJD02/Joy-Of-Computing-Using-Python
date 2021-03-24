982320fc1c1c92d86db60a81c39fcf545d90f788
egnaluD-jarivaR <redhood.dumb@gmail.com>
2021-03-24 20:23:53 +0530

    Testing out networkx lib and drawing a complete 3 node graph

---
+++ Follow_Alongs/6DegreesOfSeparation/networkAnalysis.py
@@ -0,0 +1,20 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.Graph()
+# Adding nodes
+G.add_node(1)
+G.add_node(2)
+G.add_node(3)
+# Adding Edges
+G.add_edge(1, 3)
+G.add_edge(1, 2)
+G.add_edge(2, 3)
+# Showing Nodes
+print(G.nodes())
+# Showing Edges
+print(G.edges())
+# Drawing the complete graph
+nx.draw(G)
+# Plotting on graph
+plt.show()
