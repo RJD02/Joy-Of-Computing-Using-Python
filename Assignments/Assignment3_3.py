e01f889cdb20d39dfcd5d263581ae83083395da2
egnaluD-jarivaR <redhood.dumb@gmail.com>
2021-04-09 10:23:49 +0530

    Add problem statement to the assignment

--- Assignments/Assignment3_3.py
+++ Assignments/Assignment3_3.py
@@ -1,3 +1,26 @@
+'''
+Given an array A of N integers, you have to write a python code to eliminate the duplicate integers of the array.
+
+Input Format
+
+The first line of the input contains a number N representing the number of elements in array A.
+
+The second line of the input contains N numbers separated by a space. (after the last element, there is no space)
+
+Output Format
+
+Print the resultant array of elements separated by a space. (no space after the last element)
+
+Example
+
+Input
+6
+1 2 4 2 5 4
+
+Output
+1 2 4 5
+'''
+
 def removeRepeated_1(l):
     return list(dict.fromkeys(l))
 
@@ -21,7 +44,5 @@ l = list(input().split(" "))
 #For removeRepeated_2
 l = removeRepeated_2(l)
 
-#For removeRepeated_1
-# l = removeRepeated(l)
 for i in l:
     print(i, end=" ")
