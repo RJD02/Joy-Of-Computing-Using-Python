079d5aba8887356b6ee9fcc0a2b227ab7e1cf81b
egnaluD-jarivaR <redhood.dumb@gmail.com>
2021-04-09 10:30:15 +0530

    Add problem statement to the assignment

--- Assignments/Assignment7_2.py
+++ Assignments/Assignment7_2.py
@@ -1,3 +1,31 @@
+'''
+List Prefix
+Given two lists, write a program to check if one list is a prefix of the other.
+
+Input Format
+The first line contains space separated integers
+The second line contains space separated integers
+
+Output Format
+Print Yes or No
+
+Example:
+Input:
+1 2
+1 2 3 2 1
+Output
+Yes
+
+Input:
+1 2
+1 3 2 1
+Output:
+No
+
+Explanation
+ In the first case, first list is a prefix of the second list. In the second case first list is not a prefix of the second
+'''
+
 def solve():
     l1 = input().split(" ")
     l2 = input().split(" ")
