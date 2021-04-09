b49fd777449f9c5d2e34ca4a2aff98b6897411a3
egnaluD-jarivaR <redhood.dumb@gmail.com>
2021-04-09 10:27:56 +0530

    Add problem statement to the assignment

--- Assignments/Assignment7_1.py
+++ Assignments/Assignment7_1.py
@@ -1,3 +1,24 @@
+'''
+Snakes and Ladders-1
+Given a configuration of a Snakes and Ladders board, determine the number of snakes and ladders.
+
+Input Format
+First line of the input contains the board configuration in the form of mapping of snakes and ladders
+
+Output Format
+Print two space separated integers indicating the number of snakes and ladders respectively.
+
+Example:
+Input: 
+6: 14,11: 28,17: 74,22: 7,38: 59,49: 12,57: 76,61: 54,81: 98,88: 4
+
+Output:
+4 6
+
+Explanation
+In the given configuration, 6:14 indicates a ladder and 22:7 indicates a snake. there are 4 snakes and 6 ladders.
+'''
+
 def solve():
     count_snakes = 0
     count_ladders = 0
