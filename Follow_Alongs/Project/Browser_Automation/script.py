114f22dec7d79dd74a85298248a9bb7c773e2f58
egnaluD-jarivaR <redhood.dumb@gmail.com>
2021-04-14 12:39:24 +0530

    Add functionality of contacting more than 1 person

--- Follow_Alongs/Project/Browser_Automation/script.py
+++ Follow_Alongs/Project/Browser_Automation/script.py
@@ -7,10 +7,16 @@ import time
 
 driver = webdriver.Chrome("C:\exe\chromedriver_win32\chromedriver")
 driver.get("https://web.whatsapp.com")
-wait = WebDriverWait(driver, 600)
-target = '"Soumya Dulange"' # Name of the target friend
-string = "Sent using python" # Message you want to send
-x_arg = "//span[contains(@title, " + target + ")]"
-target = wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
-target.click()
-
+wait = WebDriverWait(driver, 300)
+target = ['"Soumya Dulange"', '"Dulange😁😁😁"'] # Name of the target friend
+target_names = ['Soumya Dulange', 'Everybody in Dulange']
+count = 0
+for name in target:
+    x_arg = "//span[contains(@title, " + name + ")]"
+    name_wait = wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
+    name_wait.click()
+    string = "Hello " + target_names[count] + " This message is for you" # Message you want to send
+    input_box = driver.find_element_by_class_name("_2A8P4")
+    #  for i in range(5):
+    input_box.send_keys(string + Keys.ENTER)
+    count += 1
