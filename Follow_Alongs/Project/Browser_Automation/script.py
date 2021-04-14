b065a75ce080779cd6933d7fbd1770da64ea39a6
egnaluD-jarivaR <redhood.dumb@gmail.com>
2021-04-14 10:45:29 +0530

    Add target friend

--- Follow_Alongs/Project/Browser_Automation/script.py
+++ Follow_Alongs/Project/Browser_Automation/script.py
@@ -4,6 +4,13 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
+
 driver = webdriver.Chrome("C:\exe\chromedriver_win32\chromedriver")
 driver.get("https://web.whatsapp.com")
-wait = webDriverWait(driver, 600)
+wait = WebDriverWait(driver, 600)
+target = '"Soumya Dulange"' # Name of the target friend
+string = "Sent using python" # Message you want to send
+x_arg = "//span[contains(@title, " + target + ")]"
+target = wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
+target.click()
+
