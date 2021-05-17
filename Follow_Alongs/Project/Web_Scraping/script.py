39f3fb6752e849a6a1ff6ebbc7e22ffd230dd111
Raviraj Dulange <redhood.dumb@gmail.com>
2021-05-17 19:31:34 +0530

    Equip code to take command line arguments

--- Follow_Alongs/Project/Web_Scraping/script.py
+++ Follow_Alongs/Project/Web_Scraping/script.py
@@ -1,13 +1,16 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
+import sys
 
 driver = webdriver.Chrome('D:\\chromedriver_win32\\chromedriver')
 
 products = []
 prices = []
 ratings = []
-driver.get("https://www.flipkart.com/search?q=laptops&as=on&as-show=on&otracker=AS_Query_TrendingAutoSuggest_4_0_na_na_na&otracker1=AS_Query_TrendingAutoSuggest_4_0_na_na_na&as-pos=4&as-type=TRENDING&suggestionId=laptops&requestId=b49611cd-5f21-4e00-a591-13ff06c67f1e")
+# defaultUrl = "https://www.flipkart.com/search?q=laptops&as=on&as-show=on&otracker=AS_Query_TrendingAutoSuggest_4_0_na_na_na&otracker1=AS_Query_TrendingAutoSuggest_4_0_na_na_na&as-pos=4&as-type=TRENDING&suggestionId=laptops&requestId=b49611cd-5f21-4e00-a591-13ff06c67f1e"
+url = sys.argv[0]
+driver.get(url)
 content = driver.page_source
 soup = BeautifulSoup(content, 'html.parser')
 for a in soup.findAll('div', attrs={'class': '_2kHMtA'}):
