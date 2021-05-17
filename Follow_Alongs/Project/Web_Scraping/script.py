dbd94fabd7ff3ca0af3d4dbe1608eb01e6803486
Raviraj Dulange <redhood.dumb@gmail.com>
2021-05-17 19:24:40 +0530

    Add script/code file

---
+++ Follow_Alongs/Project/Web_Scraping/script.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
+
+driver = webdriver.Chrome('D:\\chromedriver_win32\\chromedriver')
+
+products = []
+prices = []
+ratings = []
+driver.get("https://www.flipkart.com/search?q=laptops&as=on&as-show=on&otracker=AS_Query_TrendingAutoSuggest_4_0_na_na_na&otracker1=AS_Query_TrendingAutoSuggest_4_0_na_na_na&as-pos=4&as-type=TRENDING&suggestionId=laptops&requestId=b49611cd-5f21-4e00-a591-13ff06c67f1e")
+content = driver.page_source
+soup = BeautifulSoup(content, 'html.parser')
+for a in soup.findAll('div', attrs={'class': '_2kHMtA'}):
+    name = a.find('div', attrs={'class': '_4rR01T'})
+    price = a.find('div', attrs={'class': '_30jeq3 _1_WHN1'})
+    rating = a.find('div', attrs={'class': '_3LWZlK'})
+    try:
+        if(name.text and price.text and rating.text):
+            products.append(name.text)
+            prices.append(price.text)
+            ratings.append(rating.text)
+            print(name.text)
+            print(price.text)
+            print(rating.text)
+            print('Not formatted')
+    except:
+        print('skipped')
+
+df = pd.DataFrame({'Product Name': products, 'Price': prices, 'Rating': ratings})
+df.to_csv('laptops.csv', index=False, encoding='utf-8')
